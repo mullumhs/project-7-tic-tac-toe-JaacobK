88b817f9b5188d5357b6c9b613902fea51c2bd99
JaacobK <140124151+JaacobK@users.noreply.github.com>
2024-04-04 12:40:21 +1100

    start

--- connect-four.py
+++ connect-four.py
@@ -1,24 +1,44 @@
-def main():
-    board = initialiseBoard()
-    displayBoard(board)
-    #red = 1, yellow = 2
-    currentPlayer = 1
-    
-def initialiseBoard():
-    board = []
-    for _ in range(6):
-        row = []
-        for _ in range(7):
-            row.append('-')
-        board.append(row)
-    return board
-    
-def displayBoard(board):
-    for row in board:
-        for cell in row:
-            print(cell, end=' ')
-        print()
-    print()
 
-if __name__ == "__main__":
-    main()
+board = []
+def initilise_board():
+    # have the board remeber 6 of itself that later turn into rows
+    for i in range(6):
+        board.append(["-", "-", "-", "-", "-", "-", "-", ])
+    display_board()
+
+def display_board():
+    #prints numbers for the collums
+    print(" 1 2 3 4 5 6 7")
+    for row in board:
+        #prints a '|' at the start to make more griddy as well as well as not makeing a new line
+        print("", end= '|' )
+        # gets - on its own instead of '-',
+        for collum in row:
+            #prints a | after -
+            print(collum, end= '|')
+        #goes to the next row
+        print()
+
+#def Turns():
+
+
+#def play():
+
+
+
+
+initilise_board()
+
+#palceing stuff
+count = 1
+while True:
+    token = "X"
+    if count % 2 == 0:
+        token = "O"
+    count += 1
+    print (count)
+
+    collum_choice = int(input("What Collum (1 - 7)"))
+    collum_choice -= 1
+    board[5][collum_choice] = token
+    display_board()
